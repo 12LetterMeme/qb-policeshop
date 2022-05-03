f53693a335075d88c2a401117468d94114a22c39
12LetterMeme <91357757+MaybeNotEric@users.noreply.github.com>
2022-05-03 15:15:59 -0400

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,90 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local isActive = false
+
+discord = {
+    ['webhook'] = "https://discord.com/api/webhooks/961284854887088268/aIZOMHlQtT9EB4fgH2Jo6WEDgK_eUcNHfluGjf_irUFNLj-pkIy_j22af9bnl4WTN4kD",
+    ['name'] = 'Opal Network',
+    ['image'] = "https://cdn.discordapp.com/attachments/952990680299495454/952991183779561532/opal-logo-svg.png"
+}
+
+function DiscordLog(name, message, color)
+    local embed = {
+        {
+            ["color"] = 04255,
+            ["title"] = "Police Garage Purchase Records",
+            ["description"] = message,
+            ["url"] = "https://discord.gg/opal",
+            ["footer"] = {
+            ["text"] = "By Opal Network",
+            ["icon_url"] = "https://cdn.discordapp.com/attachments/952990680299495454/952991183779561532/opal-logo-svg.png"
+        },
+            ["thumbnail"] = {
+                ["url"] = "https://cdn.discordapp.com/attachments/952990680299495454/952991183779561532/opal-logo-svg.png",
+            },
+    }
+}
+    PerformHttpRequest(discord['webhook'], function(err, text, headers) end, 'POST', json.encode({username = discord['name'], embeds = embed, avatar_url = discord['image']}), { ['Content-Type'] = 'application/json' })
+end
+
+QBCore.Functions.CreateCallback('CL-PoliceGarage:CheckIfActive', function(source, cb)
+    local src = source
+
+    if not isActive then
+        TriggerEvent("CL-PoliceGarage:server:SetActive", true)
+        cb(true)
+    else
+        cb(false)
+        TriggerClientEvent("QBCore:Notify", src, "Someone Is In The Menu Please Wait !", "error")
+    end
+end)
+
+RegisterNetEvent('CL-PoliceGarage:server:SetActive', function(status)
+    if status ~= nil then
+        isActive = status
+        TriggerClientEvent('CL-PoliceGarage:client:SetActive', -1, isActive)
+    else
+        TriggerClientEvent('CL-PoliceGarage:client:SetActive', -1, isActive)
+    end
+end)
+
+RegisterServerEvent("CL-PoliceGarage:AddVehicleSQL", function(mods, vehicle, hash, plate)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    MySQL.Async.insert('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?, ?, ?, ?, ?, ?, ?)', {
+        Player.PlayerData.license,
+        Player.PlayerData.citizenid,
+        vehicle,
+        hash,
+        json.encode(mods),
+        plate,
+        0
+    })
+end)
+
+RegisterServerEvent('CL-PoliceGarage:TakeMoney', function(data)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+    local steamname = GetPlayerName(src)
+    if Player.PlayerData.money.cash >= data.price then
+        TriggerClientEvent("CL-PoliceGarage:SpawnVehicle", src, data.vehicle)  
+        Player.Functions.RemoveMoney("cash", data.price)
+        TriggerClientEvent('QBCore:Notify', src, 'Vehicle Successfully Bought', "success")    
+        DiscordLog(discord['webhook'], 'New Vehicle Bought By: **'..steamname..'** ID: **' ..source.. '** Bought: **' ..data.vehiclename.. '** For: **' ..data.price.. '$**', 14177041) 
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'You Dont Have Enough Money !', "error")              
+    end    
+end)
+
+RegisterServerEvent('CL-PoliceGarage:TakeAirMoney', function(data)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+    local steamname = GetPlayerName(src)
+    if Player.PlayerData.money.cash >= data.price then
+        TriggerClientEvent("CL-PoliceGarage:SpawnAirVehicle", src, data.vehicle)  
+        Player.Functions.RemoveMoney("cash", data.price)
+        TriggerClientEvent('QBCore:Notify', src, 'Vehicle Successfully Bought', "success")    
+        DiscordLog(discord['webhook'], 'New Vehicle Bought By: **'..steamname..'** ID: **' ..source.. '** Bought: **' ..data.vehiclename.. '** For: **' ..data.price.. '$**', 14177041) 
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'You Dont Have Enough Money !', "error")              
+    end    
+end)
