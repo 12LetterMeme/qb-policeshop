911cd9535812b4f6080a76ec4235955e01e89fd5
12LetterMeme <91357757+MaybeNotEric@users.noreply.github.com>
2022-05-03 15:15:52 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,44 @@
+Config = Config or {}
+Config.UsePreviewMenuSync = true -- Sync for the prewview menu when player is inside the prewview menu other players cant get inside (can prevent bugs but not have to use)
+Config.UseMarkerInsteadOfMenu = false -- Want to use the marker to return the vehice? if false you can do that by opening the menu
+Config.MS = 'high' -- MS for the script recommended using high if not the "close" will get a bit baggy. options high / low
+Config.Job = 'police' --The job needed to open the menu
+
+--DO NOT add vehicles that are not in your shared ! otherwise the qb-garages wont work
+Config.Vehicles = {
+    [0] = { --grade 0
+        { ['vehiclename'] = "car 1 grade 0",         ['vehicle'] = "car1",          ['price'] = 37000},
+        { ['vehiclename'] = "car 2 grade 0",         ['vehicle'] = "car2",          ['price'] = 37000},
+    },
+    [1] = { --grade 1
+        { ['vehiclename'] = "car 1 grade 1",         ['vehicle'] = "car1",          ['price'] = 37000},
+        { ['vehiclename'] = "car 2 grade 1",         ['vehicle'] = "car2",          ['price'] = 37000},
+    },
+    [2] = { --grade 2
+    },
+    [3] = { --grade 3
+    },
+    [4] = { --grade 4
+    },
+    [5] = { --grade 5
+    },
+}
+
+Config.AirVehicles = {
+    [0]  = {--grade 0
+        { ['vehiclename'] = "car 1 grade 0",         ['vehicle'] = "heli1",         ['price'] = 37000},
+        { ['vehiclename'] = "car 1 grade 0",         ['vehicle'] = "heli2",         ['price'] = 37000},
+    },
+    [1]  = {--grade 1
+        { ['vehiclename'] = "car 1 grade 1",         ['vehicle'] = "heli1",         ['price'] = 37000},
+        { ['vehiclename'] = "car 1 grade 1",         ['vehicle'] = "heli2",         ['price'] = 37000},
+    },
+    [2]  = {--grade 2
+    },
+    [3]  = {--grade 3
+    },
+    [4]  = { --grade 4
+    },
+    [5]  = { --grade 5
+    },
+}
