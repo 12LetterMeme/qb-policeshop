7595357d016458360cd02b65b385480205374066
12LetterMeme <91357757+12LetterMeme@users.noreply.github.com>
2022-05-03 20:24:10 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 author "Edited by: 12LetterMeme#0001"
 description "A Simple Police Garage("NevoSwissa#0111")"
-version "1.1"
+version "1.0.1"
 
 client_scripts {
     'client/client.lua',
